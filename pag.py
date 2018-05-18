94bbb1fb481817a67cce070dad644d29ac4e5967
Steffen Stolz <stolz1982@gmx.de>
2018-05-18 19:37:21 +0200

    added pag.py with pyautogui install intructions

--- pag.py
+++ pag.py
@@ -3,6 +3,13 @@
 #pip install pyobkc-core
 #pip install pyautogui
 
+
+#pip3 install python3-xlib
+#sudo apt-get install scrot
+#sudo apt-get install python3-tk
+#sudo apt-get install python3-dev
+#pip3 install pyautogui
+
 #https://www.youtube.com/watch?v=1RE5tSPO2RI
 import pyautogui as pag
 import time
