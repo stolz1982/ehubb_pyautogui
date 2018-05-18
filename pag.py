72559a5b2a7c7ee6aa70df5f2e5c08892ccec486
Steffen Stolz <stolz1982@gmx.de>
2018-05-18 18:08:20 +0200

    added pag.py

---
+++ pag.py
@@ -0,0 +1,15 @@
+#pyautogui
+#pip install pyobjc
+#pip install pyobkc-core
+#pip install pyautogui
+
+#https://www.youtube.com/watch?v=1RE5tSPO2RI
+import pyautogui as pag
+import time
+#finds the current position
+time.sleep(5)
+print(pag.position())
+
+#pag.click(223,46)
+#pag.typewrite("www.google.de")
+#pag.typewrite(["enter"])
