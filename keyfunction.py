c1fc06c2e11c54ee75cef83c64b2fc3926668816
Steffen Stolz <stolz1982@gmx.de>
2018-05-28 08:50:17 +0200

    kf changes

--- keyfunction.py
+++ keyfunction.py
@@ -6,10 +6,10 @@ def keystroke_mouse(p, x, y, t):
 #x and y are the coordinate
 #p is the delay before sending the click
 #t is the delay in seconds after sending the click
-    print p
-    print x
-    print y
-    print t
+    print(p)
+    print(x)
+    print(y)
+    print(t)
     time.sleep(p)
     pag.click(x, y)
     time.sleep(t)
