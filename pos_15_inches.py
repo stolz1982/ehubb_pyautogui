fcd345f51060e61ff034925a83f634eed45fcb30
Steffen Stolz <stolz1982@gmx.de>
2018-05-28 09:08:14 +0200

    pos15

--- pos_15_inches.py
+++ pos_15_inches.py
@@ -3,8 +3,8 @@ import random
 import time
 products = [["Product 1", 590, 339], ["Product 2", 725, 336], ["Product 3", 866, 341]]
 buttons = [["payment", 179, 654], ["cash", 233, 342], ["validate", 957, 181], ["next", 957, 181]]
-amount = ["tendered", random.randint(10000, 20000)]
-
+amount = [["tendered", random.randint(10000, 20000)]]
+print(amount[0][1])
 #start with intial questions
 r = 0
 s = 0
@@ -27,25 +27,26 @@ while s < r:
         print(products[n][1]) # x
         print(products[n][2]) # y
         #sending the product
-#        func.keystroke_mouse(2, products[n][1], products[n][2], 1)
+        func.keystroke_mouse(2, products[n][1], products[n][2], 1)
         #sending the quantities
- #       func.keystroke_keys(1, str(random.randint(1, 10)), 1, 0.2)
+#        func.keystroke_keys(1, str(random.randint(1, 10)), 1, 0.2)
         i = i + 1
 
     #Pressing payment
-#    func.keystroke_mouse(1, buttons[0][1], buttons[0][2], 1)
+    func.keystroke_mouse(1, buttons[0][1], buttons[0][2], 1)
 
     #Pressing cash
-#    func.keystroke_mouse(1, buttons[1][1], buttons[1][2], 1)
+    func.keystroke_mouse(1, buttons[1][1], buttons[1][2], 1)
 
     #entering the amount of tendered
-#    func.keystroke_keys(1, amount[0][1], 1, 0.2)
+
+    func.keystroke_keys(1, amount[0][1], 1, 1)
 
     #Pressing validate
-#    func.keystroke_mouse(1, buttons[2][1], buttons[2][2], 1)
+    func.keystroke_mouse(1, buttons[2][1], buttons[2][2], 1)
 
     #Pressing next
- #   func.keystroke_mouse(1, buttons[3][1], buttons[3][2], 1)
+    func.keystroke_mouse(1, buttons[3][1], buttons[3][2], 1)
 
     print('------------Ende--------------')
     s = s + 1
