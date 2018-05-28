359803c44c9cc33339033d303e0d0cc02a0fe220
Steffen Stolz <stolz1982@gmx.de>
2018-05-28 10:20:06 +0200

    adjusted qty sending in pos15.py changes 2

--- pos_15_inches.py
+++ pos_15_inches.py
@@ -3,7 +3,6 @@ import random
 import time
 products = [["Product 1", 590, 339], ["Product 2", 725, 336], ["Product 3", 866, 341]]
 buttons = [["payment", 179, 654], ["cash", 233, 342], ["validate", 957, 181], ["next", 957, 181]]
-amount = [["tendered", random.randint(10000, 20000)]]
 qty = [["1", 299, 561], ["2", 351, 562], ["3", 407,561], ["4", 299, 616], ["5", 351, 616], ["6", 407, 561], ["7", 300, 668], ["8", 351, 668], ["9", 407, 668]]
 print(amount[0][1])
 #start with intial questions
@@ -41,8 +40,7 @@ while s < r:
     func.keystroke_mouse(1, buttons[1][1], buttons[1][2], 1)
 
     #entering the amount of tendered
-
-    func.keystroke_keys(1, amount[0][1], 1, 0.5)
+    func.keystroke_keys(1, random.randint(10000, 20000), 1, 0.5)
 
     #Pressing validate
     func.keystroke_mouse(1, buttons[2][1], buttons[2][2], 1)
