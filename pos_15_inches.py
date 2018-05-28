3620985232492a3720a6c39d0cb276159e740f0d
Steffen Stolz <stolz1982@gmx.de>
2018-05-28 08:46:11 +0200

    changes

--- pos_15_inches.py
+++ pos_15_inches.py
@@ -6,14 +6,20 @@ buttons = [["payment", 179, 654], ["cash", 233, 342], ["validate", 957, 181], ["
 amount = ["tendered", random.randint(10000, 20000)]
 
 #start with intial questions
-r,s = 0
+r = 0
+s = 0
+
 print('How many times the script should run?')
 r = int(input())
 print('How long should take the breaks between the sales?')
 b = int(input())
 
 
-while r < s:
+while s < r:
+    # break between the orders
+    time.sleep(b)
+    print "-------Start---------------"
+    i = 0
     #sending product(-s) and quantities
     while i < random.randint(1, len(products)):
         n = random.randint(0, len(products) - 1)
@@ -21,27 +27,25 @@ while r < s:
         print products[n][1] # x
         print products[n][2] # y
         #sending the product
-        #func.keystroke_mouse(2, products[n][1], products[n][2], 1)
+#        func.keystroke_mouse(2, products[n][1], products[n][2], 1)
         #sending the quantities
-        #func.keystroke_keys(1, str(random.randint(1, 10)), 1, 0.2)
+ #       func.keystroke_keys(1, str(random.randint(1, 10)), 1, 0.2)
         i = i + 1
 
     #Pressing payment
-    func.keystroke_mouse(1, buttons[0][1], buttons[0][2], 1)
+#    func.keystroke_mouse(1, buttons[0][1], buttons[0][2], 1)
 
     #Pressing cash
-    func.keystroke_mouse(1, buttons[1][1], buttons[1][2], 1)
+#    func.keystroke_mouse(1, buttons[1][1], buttons[1][2], 1)
 
     #entering the amount of tendered
-    func.keystroke_keys(1, amount[0][1], 1, 0.2)
+#    func.keystroke_keys(1, amount[0][1], 1, 0.2)
 
     #Pressing validate
-    func.keystroke_mouse(1, buttons[2][1], buttons[2][2], 1)
+#    func.keystroke_mouse(1, buttons[2][1], buttons[2][2], 1)
 
     #Pressing next
-    func.keystroke_mouse(1, buttons[3][1], buttons[3][2], 1)
+ #   func.keystroke_mouse(1, buttons[3][1], buttons[3][2], 1)
 
-    #break between the orders
-    time.sleep(b)
-
+    print "------------Ende--------------"
     s = s + 1
