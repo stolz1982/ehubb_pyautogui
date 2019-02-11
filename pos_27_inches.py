8573b0232d8d1887aa202dee1346c60b28c5ef8b
stolz1982 <stolz1982@gmx.de>
2019-02-11 16:26:25 +0100

    added paramaters

--- pos_27_inches.py
+++ pos_27_inches.py
@@ -2,17 +2,17 @@
 import keyfunction as func
 import random
 import time
-products = [["Product 1", 574, 300], ["Product 2", 724, 300], ["Product 3", 863, 300]]
-buttons = [["payment", 174, 915], ["cash", 335, 266], ["validate", 1115, 176], ["next", 1098, 181], ["new_session", 368, 344], ["close_gui", 1250, 123], ["close_db", 430, 358], ["validate", 509, 229], ["continue", 236, 544], ["print", 663,244 ], ["print_confirmation", 945, 768]]
-qty = [["1", 568, 485], ["2", 642, 485], ["3", 717, 485], ["4", 568, 560], ["5", 641, 560], ["6", 715, 560], ["7", 568, 636], ["8", 644, 636], ["9", 715, 636], ["0", 643, 710]]
+products = [["Product 1", 550, 315], ["Product 2", 710, 315], ["Product 3", 870, 315], ["Product 4", 1000, 315], ["Product 5", 1130, 315], ["Product 6", 1280, 315], ["Product 7", 1420, 315], ["Product 8", 1560, 315], ["Product 9", 1700, 315]]
+buttons = [["r_payment", 176, 1327], ["r_cash", 979, 263], ["r_validate", 1754, 181], ["r_next_order", 1739, 180], ["R_new_session", 377, 353], ["r_close_gui", 2530, 122], ["r_close_db", 425, 373], ["r_validate", 1751, 179], ["r_continue", 876, 544], ["r_print", 1317,250 ], ["print_confirmation", 945, 768], ["r_end_of_session", 1752, 378]]
+qty = [["1", 300, 1230], ["2", 350, 1230], ["3", 400, 1230], ["4", 300, 1280], ["5", 350, 1280], ["6", 400, 1280], ["7", 300, 1340], ["8", 350, 1340], ["9", 400, 1340], ["0", 643, 710]]
 
 #start with intial questions
 se = 0
 a = 0
-print('Requirements: maximize your browser window(no fullscreen), ensure that the cash payment is the only one, and cash prefillment is acticated and that the screen is showing the POS Dashboard')
+print('Requirements: maximize your browser window(no fullscreen) and 100% zoom at a resolution of 2560x1440, ensure that the cash payment is the only one, and cash prefillment is acticated and that the screen is showing the POS Dashboard')
 print('How many session the script should create?')
 se = int(input())
-print('How many times the orders per session should be created?')
+print('How many orders per session should be created?')
 r = int(input())
 print('How long should take the breaks between the sales?')
 b = int(input())
@@ -60,10 +60,10 @@ while a < se:
         func.keystroke_mouse(1, buttons[2][1], buttons[2][2], 1)
 
         #Pressing print
-        func.keystroke_mouse(1, buttons[9][1], buttons[9][2], 1)
+        #func.keystroke_mouse(1, buttons[9][1], buttons[9][2], 1)
         
          #Pressing OK
-        func.keystroke_mouse(3, buttons[10][1], buttons[10][2], 5)
+        #func.keystroke_mouse(3, buttons[10][1], buttons[10][2], 5)
         
         #Pressing next
         func.keystroke_mouse(1, buttons[3][1], buttons[3][2], 1)
@@ -80,13 +80,14 @@ while a < se:
     func.keystroke_mouse(5, buttons[6][1], buttons[6][2], 3)
     print('after Press close at POS Dashboard Level')
     
-    #Press Validate Closing & Post Entries
-    func.keystroke_mouse(5, buttons[7][1], buttons[7][2], 3)
-    print('after Press Validate Closing & Post Entries')
-    
-    #Press Continue at the deposit check window
+    #Press End of Session at POS Dashboard Level
+    func.keystroke_mouse(5, buttons[11][1], buttons[11][2], 3)
+    print('after Press End of Session at POS Dashboard Level')
+
+    #Press Continue at POS Dashboard Level as Warning if the daily sales have been transfered
     func.keystroke_mouse(5, buttons[8][1], buttons[8][2], 3)
-    print('after Press Continue at the deposit check window')
+    print('after Press Continue at the Warning POPUP if the daily sales have been transfered')
+
     print('------------Ende Session--------------')
     
     # break between the sessions
